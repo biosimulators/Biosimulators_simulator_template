af8fe347222043feaaa0a1d7b92fb001edc9484b
Jonathan Karr <jonrkarr@gmail.com>
2020-12-12 19:08:31 -0500

    updating test template

--- tests/test_all.py
+++ tests/test_all.py
@@ -5,22 +5,20 @@
 :Copyright: YYYY, Owner
 :License: <License, e.g., MIT>
 """
-
-from biosimulations_utils.simulator.testing import SimulatorValidator
+from biosimulators_utils.simulator.exec import exec_sedml_docs_in_archive_with_containerized_simulator
 from my_simulator import __main__
-import my_simulator
-import capturer
-import docker
+from my_simulator import core
+from unittest import mock
+import numpy
 import os
-import PyPDF2
 import shutil
 import tempfile
 import unittest
 
 
 class CliTestCase(unittest.TestCase):
-    EXAMPLE_ARCHIVE_FILENAME = 'tests/fixtures/BIOMD0000000297.omex'
-    DOCKER_IMAGE = '<organization>/<repository>'
+    EXAMPLE_ARCHIVE_FILENAME = os.path.join(os.path.dirname(__file__), 'fixtures', 'BIOMD0000000297.omex')
+    DOCKER_IMAGE = '<registry>/<organization>/<repository>'
 
     def setUp(self):
         self.dirname = tempfile.mkdtemp()
@@ -28,93 +26,25 @@ class CliTestCase(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.dirname)
 
-    def test_help(self):
-        with self.assertRaises(SystemExit):
-            with __main__.App(argv=['--help']) as app:
-                app.run()
+    def test_raw_cli(self):
+        with mock.patch('sys.argv', ['', '--help']):
+            with self.assertRaises(SystemExit) as context:
+                __main__.main()
+                self.assertRegex(context.Exception, 'usage: ')
 
-    def test_version(self):
-        with __main__.App(argv=['-v']) as app:
-            with capturer.CaptureOutput(merged=False, relay=False) as captured:
-                with self.assertRaises(SystemExit):
-                    app.run()
-                self.assertIn(my_simulator.__version__, captured.stdout.get_text())
-                self.assertEqual(captured.stderr.get_text(), '')
+    def test_exec_sedml_docs_in_combine_archive(self):
+        core.exec_sedml_docs_in_combine_archive(self.EXAMPLE_ARCHIVE_FILENAME, self.dirname)
+        self.assert_outputs_created()
 
-        with __main__.App(argv=['--version']) as app:
-            with capturer.CaptureOutput(merged=False, relay=False) as captured:
-                with self.assertRaises(SystemExit):
-                    app.run()
-                self.assertIn(my_simulator.__version__, captured.stdout.get_text())
-                self.assertEqual(captured.stderr.get_text(), '')
-
-    def test_sim_short_arg_names(self):
+    def test_exec_sedml_docs_in_combine_archive_with_cli(self):
         with __main__.App(argv=['-i', self.EXAMPLE_ARCHIVE_FILENAME, '-o', self.dirname]) as app:
             app.run()
-        self.assert_outputs_created(self.dirname)
+        self.assert_outputs_created()
 
-    def test_sim_long_arg_names(self):
-        with __main__.App(argv=['--archive', self.EXAMPLE_ARCHIVE_FILENAME, '--out-dir', self.dirname]) as app:
-            app.run()
-        self.assert_outputs_created(self.dirname)
-
-    def test_build_docker_image(self):
-        docker_client = docker.from_env()
-        docker_client.images.build(
-            path='.',
-            dockerfile='Dockerfile',
-            pull=True,
-            rm=True,
-        )
-
-    def test_sim_with_docker_image(self):
-        docker_client = docker.from_env()
-
-        # setup input and output directories
-        in_dir = os.path.join(self.dirname, 'in')
-        out_dir = os.path.join(self.dirname, 'out')
-        os.makedirs(in_dir)
-        os.makedirs(out_dir)
-
-        # copy model and simulation to temporary directory which will be mounted into container
-        shutil.copyfile(self.EXAMPLE_ARCHIVE_FILENAME, os.path.join(in_dir, os.path.basename(self.EXAMPLE_ARCHIVE_FILENAME)))
-
-        # run image
-        docker_client.containers.run(
-            image=self.DOCKER_IMAGE,
-            volumes={
-                in_dir: {
-                    'bind': '/root/in',
-                    'mode': 'ro',
-                },
-                out_dir: {
-                    'bind': '/root/out',
-                    'mode': 'rw',
-                }
-            },
-            command=['-i', '/root/in/' + os.path.basename(self.EXAMPLE_ARCHIVE_FILENAME), '-o', '/root/out'],
-            tty=True,
-            remove=True)
-
-        self.assert_outputs_created(out_dir)
+    def test_exec_sedml_docs_in_combine_archive_with_docker_image(self):
+        exec_sedml_docs_in_archive_with_containerized_simulator(
+            self.EXAMPLE_ARCHIVE_FILENAME, self.out_dir, self.DOCKER_IMAGE, pull_docker_image=False)
+        self.assert_outputs_created()
 
     def assert_outputs_created(self, dirname):
-        self.assertEqual(set(os.listdir(dirname)), set(['ex1', 'ex2']))
-        self.assertEqual(set(os.listdir(os.path.join(dirname, 'ex1'))), set(['BIOMD0000000297']))
-        self.assertEqual(set(os.listdir(os.path.join(dirname, 'ex2'))), set(['BIOMD0000000297']))
-        self.assertEqual(set(os.listdir(os.path.join(dirname, 'ex1', 'BIOMD0000000297'))), set(['plot_1_task1.pdf', 'plot_3_task1.pdf']))
-        self.assertEqual(set(os.listdir(os.path.join(dirname, 'ex2', 'BIOMD0000000297'))), set(['plot_1_task1.pdf', 'plot_3_task1.pdf']))
-
-        files = [
-            os.path.join(dirname, 'ex1', 'BIOMD0000000297', 'plot_1_task1.pdf'),
-            os.path.join(dirname, 'ex1', 'BIOMD0000000297', 'plot_3_task1.pdf'),
-            os.path.join(dirname, 'ex2', 'BIOMD0000000297', 'plot_1_task1.pdf'),
-            os.path.join(dirname, 'ex2', 'BIOMD0000000297', 'plot_3_task1.pdf'),
-        ]
-        for file in files:
-            with open(file, 'rb') as file:
-                PyPDF2.PdfFileReader(file)
-
-    def test_validator(self):
-        validator = SimulatorValidator()
-        validator.run(self.DOCKER_IMAGE)
+        self.assertEqual(set(os.listdir(dirname)), set(['reports.h5']))
